27ff3ec9259491cfdd3c93f72b81a473f4298d70
Bhaswati Roy <roybhaswati24@gmail.com>
2023-03-07 19:16:16 +0530

    Update docbokeh.py

--- docbokeh.py
+++ docbokeh.py
@@ -1,6 +1,8 @@
-''' A categorical dot plot based on simple Python lists of data.
+''' A interactive numerical tap plot based on simple Python array of data.
+    One particiular datapoint can be focused by tapping on it. 
+    This highlights that datapoint, and displays other datapoints in faded color.
 .. bokeh-example-metadata::
     :apis: bokeh.plotting.figure.circle, bokeh.plotting.figure.segment
-    :refs: :ref:`ug_basic_bars`
-    :keywords: circle, dotplot, segments
+    :refs: :ref:`ug_basic_tap`
+    :keywords: circle, tapplot, segments, tap
 '''
